acf1fd3c5d1a0be0bb6f860e0141c0047db7560e
h3cat3 <dumitrescu.c.e@gmail.com>
2026-09-06 15:28:51 +0100

    mobile menu
    styled the mobile menu in scss and created the js to toggle it

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
-const dropdownBtn = document.querySelectorAll('.nav__item--has-dropdown');
-const dropdownCard = document.querySelectorAll('.nav__dropdown');
-const dropdownArrow = document.querySelectorAll('.nav__arrow');
+const desktopDropdownBtn = document.querySelectorAll('.nav__item--has-dropdown');
+const desktopDropdownCard = document.querySelectorAll('.nav__dropdown');
+const desktopDropdownArrow = document.querySelectorAll('.nav__arrow');
 
-dropdownBtn.forEach((btn, index) => {
-  btn.addEventListener('click', () => {
-    const isExpanded = btn.getAttribute('aria-expanded') !== 'true'; 
-    dropdownArrow[index].src = isExpanded ? './images/icon-arrow-up.svg' : './images/icon-arrow-down.svg';
-    btn.setAttribute('aria-expanded', isExpanded);
-    dropdownCard[index].classList.toggle('hidden');
-  });
+function toggleDropdowns(dropdownBtn, dropdownCard, dropdownArrow) {
+    dropdownBtn.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+        const isExpanded = btn.getAttribute('aria-expanded') !== 'true'; 
+        dropdownArrow[index].src = isExpanded ? './images/icon-arrow-up.svg' : './images/icon-arrow-down.svg';
+        btn.setAttribute('aria-expanded', isExpanded);
+        dropdownCard[index].classList.toggle('hidden');
+    });
+    });
+}
+//open/close desktop dropdowns
+toggleDropdowns(desktopDropdownBtn, desktopDropdownCard, desktopDropdownArrow); 
+//mobile menu
+const mobileMenuOpenBtn = document.querySelector('.nav__toggle');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileMenuCloseBtn = document.querySelector('.mobile-menu__close');
+const mobileDropdownBtn = document.querySelectorAll('.mobile-menu__item--has-dropdown');
+const mobileDropdownCard = document.querySelectorAll('.mobile-menu__dropdown');
+const mobileDropdownArrow = document.querySelectorAll('.mobile-menu__arrow');   
+//open mobile menu
+mobileMenuOpenBtn.addEventListener('click', () => {
+  mobileMenu.classList.remove('hidden');
+  mobileMenu.setAttribute('aria-hidden', 'false');
 });
+//close mobile menu
+mobileMenuCloseBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('hidden');
+  mobileMenu.setAttribute('aria-hidden', 'true');
+});
+//mobile menu open/close dropdown
+toggleDropdowns(mobileDropdownBtn, mobileDropdownCard, mobileDropdownArrow);    
