90fe69ac2635356cfcb5f5c56bc717cd18810aa9
h3cat3 <dumitrescu.c.e@gmail.com>
2026-09-08 18:08:19 +0100

    fixing JS, font and button errors
    Fixing "Careers" or "About" errors - added a different class to the buttons so the anchors are not selected
    Fixing the font error
    Close all dropdown when clicked outside and when 'escape' is pressed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const desktopDropdownBtn = document.querySelectorAll('.nav__link');
+const desktopDropdownBtn = document.querySelectorAll('.nav__link-btn');
 const desktopDropdownCard = document.querySelectorAll('.nav__dropdown');
 const desktopDropdownArrow = document.querySelectorAll('.nav__arrow');
 
@@ -12,24 +12,51 @@ function toggleDropdowns(dropdownBtn, dropdownCard, dropdownArrow) {
     });
     });
 }
+function closeAllDropdowns(dropdownBtn, dropdownCard, dropdownArrow) {
+  dropdownBtn.forEach((btn,index) => {
+    dropdownArrow[index].src = './images/icon-arrow-down.svg';
+    btn.setAttribute('aria-expanded', 'false');
+    dropdownCard[index].classList.add('hidden');
+  })
+}
 //open/close desktop dropdowns
 toggleDropdowns(desktopDropdownBtn, desktopDropdownCard, desktopDropdownArrow); 
+//close descktop dropdowns
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.nav__item--has-dropdown')){
+    closeAllDropdowns(desktopDropdownBtn, desktopDropdownCard, desktopDropdownArrow); 
+  }
+})
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape') closeAllDropdowns(desktopDropdownBtn, desktopDropdownCard, desktopDropdownArrow);
+})
 //mobile menu
 const mobileMenuOpenBtn = document.querySelector('.nav__toggle');
 const mobileMenu = document.getElementById('mobile-menu');
 const mobileMenuCloseBtn = document.querySelector('.mobile-menu__close');
-const mobileDropdownBtn = document.querySelectorAll('.mobile-menu__link');
+const mobileDropdownBtn = document.querySelectorAll('.mobile-menu__link-btn');
 const mobileDropdownCard = document.querySelectorAll('.mobile-menu__dropdown');
 const mobileDropdownArrow = document.querySelectorAll('.mobile-menu__arrow');   
 //open mobile menu
 mobileMenuOpenBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('hidden');
   mobileMenu.setAttribute('aria-hidden', 'false');
+  mobileMenuOpenBtn.setAttribute('aria-expanded', 'true');
+  mobileMenuOpenBtn.setAttribute('aria-label', 'close menu');
 });
 //close mobile menu
 mobileMenuCloseBtn.addEventListener('click', () => {
   mobileMenu.classList.add('hidden');
   mobileMenu.setAttribute('aria-hidden', 'true');
+  mobileMenuOpenBtn.setAttribute('aria-expanded', 'false');
+  mobileMenuOpenBtn.setAttribute('aria-label', 'open menu');
 });
 //mobile menu open/close dropdown
 toggleDropdowns(mobileDropdownBtn, mobileDropdownCard, mobileDropdownArrow);    
+
+//close dropdown when click outside
+document.addEventListener('click', (e) => {
+  if (!e.target.closest('.mobile-menu__item--has-dropdown')){
+    closeAllDropdowns(mobileDropdownBtn, mobileDropdownCard, mobileDropdownArrow);
+  }
+})
